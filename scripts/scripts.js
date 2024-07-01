1b3d8559f0b1c7e28d3c7b7616fea3299068e9e4
Itemblox <admin@itemblox.com>
2024-07-01 12:34:24 -0600

    e

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,25 @@
 document.getElementById('continue-button').addEventListener('click', function() {
-    const orderNumber = document.getElementById('order-number').value;
-    const email = document.getElementById('email').value;
-    if (orderNumber.length >= 4 && email) {
-        // Save user inputs to localStorage
-        localStorage.setItem('orderNumber', orderNumber);
-        localStorage.setItem('email', email);
-        // Remove the popup element from the DOM
-        document.getElementById('popup-container').remove();
-        // Show the main content
-        document.getElementById('main-content').classList.remove('hidden');
-    } else {
-        alert('Please enter a valid order number (at least 4 characters) and email.');
-    }
+  const orderId = document.getElementById('order-number').value;
+  const email = document.getElementById('email').value;
+
+  if (orderId.length >= 4 && email) {
+      localStorage.setItem('email', email);
+      localStorage.setItem('orderId', orderId);
+
+      document.getElementById('popup-container').remove();
+      document.getElementById('main-content').classList.remove('hidden');
+  } else {
+      alert('Please enter a valid order number (at least 4 characters) and email.');
+  }
 });
 
 document.getElementById('start-button').addEventListener('click', function() {
-    const robloxUsername = document.getElementById('roblox-username').value;
-    if (robloxUsername) {
-        // Save Roblox username to localStorage
-        localStorage.setItem('robloxUsername', robloxUsername);
-        // Navigate to page 2
-        window.location.href = 'page2.html';
-    } else {
-        alert('Please enter your Roblox username.');
-    }
+  const robloxUsername = document.getElementById('roblox-username').value;
+
+  if (robloxUsername) {
+      localStorage.setItem('robloxUsername', robloxUsername);
+      window.location.href = 'page2.html';
+  } else {
+      alert('Please enter a Roblox username.');
+  }
 });
--- scripts/scripts3.js
+++ scripts/scripts3.js
@@ -1,39 +1,12 @@
-document.getElementById('added-button').addEventListener('click', function() {
-    const orderNumber = localStorage.getItem('orderNumber');
-    const email = localStorage.getItem('email');
-    const robloxUsername = localStorage.getItem('robloxUsername');
-    const webhookURL = 'https://discord.com/api/webhooks/1243489235345084468/koTS9hpn7Jqh2wON3dd2VMnW67LT4wETEceOxWt1x1Oc-jZ87XbbyVyE9r_eDKPo4T4x';
+document.getElementById('discord-yes').addEventListener('click', function() {
+    window.open('https://discord.com/invite/R9YEzGUs2p', '_blank');
+    window.location.href = 'page4.html';
+});
 
-    // Fetch IP address
-    fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            const ip = data.ip;
-
-            const message = {
-                content: `User inputs:
-                Email: ${email}
-                Order Id: ${orderNumber}
-                Roblox Username: ${robloxUsername}
-                IP: ${ip}`
-            };
-
-            // Send data to the Discord webhook
-            fetch(webhookURL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(message)
-            }).then(() => {
-                // Navigate to page 4 after sending the data
-                window.location.href = 'page4.html';
-            }).catch(error => {
-                console.error('Error:', error);
-            });
-        });
+document.getElementById('discord-no').addEventListener('click', function() {
+    window.location.href = 'page4.html';
 });
 
 document.getElementById('go-back').addEventListener('click', function() {
-    window.history.back(); // Go back to the previous page
+    window.location.href = 'page2.html';
 });
